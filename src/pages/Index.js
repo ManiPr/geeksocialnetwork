9df874394944d1fafbdcaa5116d1eb846dfe3820
ManiGeek <maniweeb@gmail.com>
2024-05-07 11:17:36 +0330

    feat:folderstructure

---
+++ src/pages/Index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+import "./Index.css";
+
+export default function Index() {
+
+  return (
+    <>
+    <p>fjfkldsajfklsjdfk;kfjak;lfjklsdjkf</p>
+    </>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Index from "./pages/Index"
+
+
+const routes = [
+    { path: '/b', element: <Index /> },
+]
+
+export default routes
